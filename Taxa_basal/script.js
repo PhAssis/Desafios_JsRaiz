427a3c67b41be06ff0095ff9fab556bd0aa8f87a
PhAssis <paulo_hda@hotmail.com>
2020-11-12 06:04:23 -0300

    fit:Finalizado funcao de calcular o TMB e funcao de inserir na DOM

--- Taxa_basal/script.js
+++ Taxa_basal/script.js
@@ -1,41 +1,38 @@
 const inputHeight = document.querySelector('.altura');
 const inputWeight = document.querySelector('.peso');
 const inputAge = document.querySelector('.idade');
-const inputGenre = document.querySelectorAll('input[name=genero]');
+const inputGenre = document.querySelectorAll('input[name=genre]');
 const button = document.querySelector('.btn');
 const container = document.querySelector('.container');
 const messageResult = document.querySelector('.messageResult');
 
-const inputHeightValue = inputHeight.value;
-const inputWeightValue = inputWeight.value;
-const inputAgeValue = inputAge.value;
-
-
-
-
-function calculaTmb(genero) {
-  
-  const formulaPartOne = `${genero === 'masculino' ? 66.5 : 665.1}`;
-  const formulaPartTwo = (`${genero === 'masculino' ? 13.75 : 9.56}` * inputWeightValue);
-  const formulaPartThree = (`${genero === 'masculino' ? 5.0 : 1.8}` * inputHeightValue);
-  const formulaPartFour = (`${genero === 'masculino' ? 6.8 : 4.7}` * inputAgeValue);
+function calculaTmb(genre, height, weight, age) {
+  const formulaPartOne = parseFloat(`${genre === 'masculino' ? 66.5 : 665.1}`);
+  const formulaPartTwo = (`${genre === 'masculino' ? 13.75 : 9.56}` * weight);
+  const formulaPartThree = (`${genre === 'masculino' ? 5.0 : 1.8}` * height);
+  const formulaPartFour = (`${genre === 'masculino' ? 6.8 : 4.7}` * age);
 
   const formulaResult = formulaPartOne + formulaPartTwo + formulaPartThree - formulaPartFour;
-  
-  return formulaResult;  
+
+  return formulaResult;
 }
 
-  
-function insertResult() {
+function insertResult(genre, height, weight, age) {
   messageResult.innerHTML = '';
-  messageResult.innerHTML = 
-  `<div class="alert alert-primary text-center" role="alert">
-      Você precisa de ${calculaTmb(inputGenreValue)} calorias diária.
+  messageResult.innerHTML =
+    `<div class="alert alert-primary text-center" role="alert">
+      Você precisa de ${calculaTmb(genre, height, weight, age)} calorias diária.
     </div>`;
 }
 
-button.addEventListener('click', () => {
+function resultTMB() {
+  const inputHeightValue = inputHeight.value;
+  const inputWeightValue = inputWeight.value;
+  const inputAgeValue = inputAge.value;
+  
   const inputGenreValue =
-    [...inputGenre].filter(input => input.checked ? input.name: '');
-  console.log(inputGenreValue)
-})
+    [...inputGenre].filter(input => input.checked)[0].value;
+  insertResult(inputGenreValue, inputHeightValue, inputWeightValue, inputAgeValue);
+}
+
+button.addEventListener('click', resultTMB);
