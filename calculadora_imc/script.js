7d6f329799a5d4639547d81eb3d6fd86b3a2414c
PhAssis <paulo_hda@hotmail.com>
2020-11-15 12:27:02 -0300

    fit: AJustando funcoes

--- calculadora_imc/script.js
+++ calculadora_imc/script.js
@@ -22,41 +22,50 @@ const { alertDangerMessage,
   gradeObesityOne
 } = optionsMessage;
 
+const getValueOfUnderWeight = valueIndexImc =>
+  calculatedValueImc => calculatedValueImc < valueIndexImc;
+
+const getValueOfGradeObesityThree = valueIndexImc =>
+  calculatedValueImc => calculatedValueImc < valueIndexImc;
+
+const getBetweenValues = (valueIndexImcOne, valueIndexImcTwo) =>
+  calculatedValueImc => calculatedValueImc >= valueIndexImcOne && calculatedValueImc < valueIndexImcTwo;
+
 const informationsImc = [
-  [valor => valor < 18.5, underWeight],
-  [valor => valor > 18.5 && valor < 24.9, normalWeight],
-  [valor => valor >= 25 && valor < 29.9, overweight],
-  [valor => valor >= 30 && valor < 34.9, gradeObesityOne],
-  [valor => valor >= 35 && valor < 39.9, gradeObesityTwo],
-  [valor => valor >= 40, gradeObesityThree]
+  [getValueOfUnderWeight(18.5), underWeight],
+  [getBetweenValues(18.5, 24.9), normalWeight],
+  [getBetweenValues(25, 29.9), overweight],
+  [getBetweenValues(30, 34.9), gradeObesityOne],
+  [getBetweenValues(35, 39.9), gradeObesityTwo],
+  [getValueOfGradeObesityThree(40), gradeObesityThree]
 ];
 
-function calculationImc(weight, height) {
-  const calculatedImc = weight / (height * height)
-  const resultImc = informationsImc.find(fn => fn[0](calculatedImc))[1]
-  return resultImc;
-}
+const calculationImc = (weight, height) => weight / (height * height);
 
-const typeMessage = (resultImc) =>
+const selectTheMessage = (weight, height) => informationsImc.find(([fn]) => fn(calculationImc(weight, height)));
+
+const typeMessage = resultImc =>
   Object.values(alertDangerMessage).includes(resultImc)
     ? 'danger'
     : 'warning';
 
-function insertResultIntoDom(resultImc) {
+const resultMessage = resultImc =>
+  `<div class="alert alert-${resultImc === normalWeight ? 'primary' : typeMessage(resultImc)} text-center" role="alert">
+    ${resultImc}
+   `;
+
+function insertResultMessageIntoDom(resultImc) {
   messageResult.innerHTML = '';
-  messageResult.innerHTML =
-    `<div class="alert alert-${resultImc === normalWeight ? 'primary' : typeMessage(resultImc)} text-center" role="alert">
-      ${resultImc}
-    `;
+  messageResult.innerHTML = resultMessage(resultImc)
 }
 
 function resultImc() {
   const inputHeightValue = inputHeight.value;
   const inputWeightValue = inputWeight.value;
-  insertResultIntoDom(calculationImc(inputWeightValue, inputHeightValue));
+  insertResultMessageIntoDom(selectTheMessage(inputWeightValue, inputHeightValue));
 }
 
-button.addEventListener('click', resultImc)
+button.addEventListener('click', resultImc);
 
 
 
